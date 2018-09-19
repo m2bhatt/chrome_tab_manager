e6ccdda9459b5eb1f483cdbc54b2650ff87067be
Megha Bhatt <megha.bhatt@shopify.com>
2018-09-19 19:01:37 -0400

    Render list of open tabs when extension loads

--- src/popup.ts
+++ src/popup.ts
@@ -2,4 +2,13 @@ import * as moment from 'moment';
 import * as $ from 'jquery';
 
 $(function() {
+  function displayUrlsOfOpenTabs() {
+    chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+      tabs.forEach((tab) => {
+        $('#open-tabs').append(`<li>${tab.title}</li>`)
+      });
+    });
+  }
+
+  displayUrlsOfOpenTabs();
 });
