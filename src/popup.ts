51a9f14e3f0a8ccc262ef06ced9ddc8b681a5422
Megha Bhatt <megha.bhatt@shopify.com>
2018-10-04 23:26:22 -0400

    Added event handler for highlighting selected item

--- src/popup.ts
+++ src/popup.ts
@@ -5,10 +5,20 @@ $(function() {
   function displayUrlsOfOpenTabs() {
     chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
       tabs.forEach((tab) => {
-        $('#open-tabs').append(`<li class="title">${tab.title}</li><li class="link">${tab.url}</li>`)
+        var $wrapper = $("<div class='container'></div>"); 
+        var $image = $(`<img class="favicon" src="${tab.favIconUrl}">`).appendTo($wrapper); 
+        var $title = $(`<li class="title">${tab.title}</li>`).appendTo($wrapper);
+        var $url = $(`<li class="link">${tab.url}</li>`).appendTo($wrapper);
+
+        $wrapper.click(onWrapperClick);
+        $('#open-tabs').append($wrapper);
       });
     });
   }
-
   displayUrlsOfOpenTabs();
+
+  function onWrapperClick(event) {
+    var index = $(`.${event.target.className}`).index(event.target);
+    chrome.tabs.highlight({tabs:[index]});
+  }
 });
