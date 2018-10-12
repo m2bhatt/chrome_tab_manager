beca91fe9828caf4c2f39da7220f34256e07132f
Megha Bhatt <megha.bhatt@shopify.com>
2018-10-12 17:41:33 -0400

    Added tests

--- src/popup.ts
+++ src/popup.ts
@@ -1,18 +1,15 @@
 import * as moment from 'moment';
 import * as $ from 'jquery';
+import Tab from './tab';
 
 $(function() {
   function displayUrlsOfOpenTabs() {
     chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
       tabs.forEach((tab) => {
-        var $wrapper = $("<div class='container'></div>"); 
-        var $li = $("<li class='subcontainer'></li>").appendTo($wrapper);
-        var $image = $(`<img class="favicon" src="${tab.favIconUrl}">`).appendTo($li); 
-        var $title = $(`<text class="title">${tab.title}</text>`).appendTo($li);
-        var $url = $(`<li class="link">${tab.url}</li>`).appendTo($wrapper);
+        var $tabInfo = new Tab(tab).render($);
+        $tabInfo.click(onWrapperClick);
 
-        $wrapper.click(onWrapperClick);
-        $('#open-tabs').append($wrapper);
+        $('#open-tabs').append($tabInfo);
       });
     });
   }
