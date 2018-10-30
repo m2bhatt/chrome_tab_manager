2feb8342aec1cb68ded069f9cc69e33dc8370455
Megha Bhatt <megha.bhatt@shopify.com>
2018-10-29 20:58:05 -0400

    All tests pass

--- src/tab.test.ts
+++ src/tab.test.ts
@@ -23,19 +23,21 @@ test('can be instantiated from a Chrome Tab', () => {
 
 test('it renders a wrapper', () => {
     let tab = new Tab(createChromeTab());
-    expect(tab.render($)[0].tagName).toBe('DIV')
+    expect(tab.render($)[0].tagName).toBe('DIV');
 });
 
 test('it renders the title of the tab', () => {
     let tab = new Tab(createChromeTab());
-    expect($('li text', tab.render($)).text()).toBe('Google')
+    expect($('li text', tab.render($)).text()).toBe('Google');
 });
 
 test('it renders the fav icon of the tab', () => {
     let tab = new Tab(createChromeTab());
-    expect($('li img', tab.render($))[0]["src"]).toBe('https://google.com/fav.ico')
+    expect($('li img', tab.render($))[0]["src"]).toBe('https://google.com/fav.ico');
 });
 
 test('it renders the url', () => {
-    // to do
+    let tab = new Tab(createChromeTab());
+    // $('li', tab.render($))[1] returns `undefined`?
+    expect($('li', tab.render($)).eq(1).text()).toBe('google.com');
 });
