6fbb624a9ada9b791478d0a86b41d1f8784d567c
Megha Bhatt <megha.bhatt@shopify.com>
2018-11-13 17:57:50 -0500

    Added closing and pinning tabs functionality

--- src/popup.ts
+++ src/popup.ts
@@ -2,12 +2,15 @@ import * as moment from 'moment';
 import * as $ from 'jquery';
 import Tab from './tab';
 
+var tabs;
+
 $(function() {
   function displayUrlsOfOpenTabs() {
     chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+      tabs = tabs;
       tabs.forEach((tab) => {
         var $tabInfo = new Tab(tab).render($);
-        $tabInfo.click(onWrapperClick);
+      //  $tabInfo.click(onWrapperClick);
 
         $('#open-tabs').append($tabInfo);
       });
@@ -19,4 +22,9 @@ $(function() {
     var index = $(`.${event.target.className}`).index(event.target);
     chrome.tabs.highlight({tabs:[index]});
   }
+
+  // function onDeleteClick(event) {
+  //   var index = $(":button").index(event.target);
+  //   chrome.tabs.remove({tabs:[index]});
+  // }
 });
--- src/tab.ts
+++ src/tab.ts
@@ -10,8 +10,20 @@ export default class Tab {
         var $li = $("<li class='subcontainer'></li>").appendTo($wrapper);
         var $image = $(`<img class="favicon" src="${this.chromeTab.favIconUrl}">`).appendTo($li); 
         var $title = $(`<text class="title">${this.chromeTab.title}</text>`).appendTo($li);
+        var $delTab = $('<input class="delTab" type="button" value="x"></input>').appendTo($li);
+        $delTab.click(this.close.bind(this, $wrapper));
+        var $pinTab = $('<input class="pinTab" type="button" value="p"></input>').appendTo($li);
+        $pinTab.click(this.pin.bind(this));
         var $url = $(`<li class="link">${this.chromeTab.url}</li>`).appendTo($wrapper);
 
         return $wrapper;
+    }
+
+    close(element) {
+        chrome.tabs.remove(this.chromeTab.id, () => element.remove());
+    }
+
+    pin() {
+        chrome.tabs.update(this.chromeTab.id, {pinned: true})  
     }
 }
