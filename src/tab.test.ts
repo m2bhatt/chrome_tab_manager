1e8ad2e158c95999ed55317e4d8cdd0dc1d12a73
Megha Bhatt <meghabhatt@Meghas-MacBook-Pro.local>
2018-11-14 21:47:35 -0500

    Add tests

--- src/tab.test.ts
+++ src/tab.test.ts
@@ -2,8 +2,8 @@ import Tab from './tab';
 import * as jQuery from 'jquery';
 import { JSDOM } from 'jsdom';
 
-function createTab() {
-    return new Tab(createChromeTab(), {chrome: {tabs: {}}})
+function createTab(tabs = {}) {
+    return new Tab(createChromeTab(), {chrome: {tabs: tabs}})
 }
 
 function createChromeTab() {
@@ -44,4 +44,30 @@ test('it renders the url', () => {
     let tab = createTab();
     // $('li', tab.render($))[1] returns `undefined`?
     expect($('li', tab.render($)).eq(1).text()).toBe('google.com');
+});
+
+test('it can pin a tab', () => {
+    let tabs = {update: jest.fn()};
+    let tab = createTab(tabs);
+
+    $('li .pinTab', tab.render($)).click();
+
+    expect(tabs.update).toBeCalledTimes(1);
+    // Try to update the expecation below to use the .toBeCalledWith matcher
+    expect(tabs.update.mock.calls[0][1]).toEqual({pinned: true});
+});
+
+test('it can close a tab', () => {
+    let tabs = {remove: jest.fn()};
+    let tab = createTab(tabs); 
+
+    $('li .delTab', tab.render($)).click();
+
+    expect(tabs.remove).toBeCalledTimes(1);
+    // Test that the element has been removed from the DOM (there is a gotcha: 
+    // you need to invoke the callback function passed to the mock to simulate the behaviour of the Chrome API)
+});
+
+test('it can unpin a tab', () => {
+
 });
