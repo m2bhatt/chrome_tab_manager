6da436bde05eb66fbfdf53153b5d23a1eb7561e0
Megha Bhatt <megha.bhatt@shopify.com>
2018-11-28 17:23:30 -0500

    Introduce TabCollection with simple search functionality

---
+++ src/tab_collection.ts
@@ -0,0 +1,38 @@
+import Tab, {ChromeTab} from './tab';
+//import Renderer from './renderer'
+
+export default class TabCollection {
+    readonly tabs: Tab[];
+   // readonly length: number;
+    readonly options;
+
+    constructor(tabs: ChromeTab[], options = { chrome: { tabs: null } }) {
+        this.options = options;
+        this.tabs = [];
+        tabs.forEach((tab) => {
+            this.tabs.push(new Tab(tab, options));
+        });
+       // this.length = this.tabs.length;
+    }
+
+    length() {
+        return this.tabs.length;
+    };
+
+    search(query) {
+        let tabCollection = new TabCollection ([], this.options);
+        for (let tab of this.tabs) {
+            if (tab.title.indexOf(query) !== -1) {
+                tabCollection.tabs.push(tab);
+            } 
+        }
+        return tabCollection;
+    }
+
+    render($) {
+        this.tabs.forEach((tab) => {
+            var $tabInfo = tab.render($);
+            $('#open-tabs').append($tabInfo);
+        });
+    }
+}
